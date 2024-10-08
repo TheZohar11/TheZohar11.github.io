49121663b1fd5e8d1fb03dfa1a80d8f001617cc8
TheZohar11 <163902267+TheZohar11@users.noreply.github.com>
2024-10-09 02:24:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// Get modal elements
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("contactBtn");
+const span = document.querySelector(".close-btn");
+
+// Open modal when button is clicked
+btn.onclick = function () {
+  modal.style.display = "flex"; // Display modal with flexbox
+};
+
+// Close modal when the "x" button is clicked
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Close modal if clicking outside the modal content
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
